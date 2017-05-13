21b202ff3a3287be4066674df89febaf263cd651
Will McKenzie <willmckenzie@oinutter.co.uk>
2017-05-13 11:18:04 +0100

    Loop some colors

--- neopixel/saber.py
+++ neopixel/saber.py
@@ -7,8 +7,15 @@ np = neopixel.NeoPixel(pin1, 60)
 
 ROWS = 60
 COLS = 1
+GAP = 50
 pos = 0
-color = 50
+color = 85
+color_index = 0
+
+def get_next_color():
+    foo = [50,50,50]
+    foo[color_index] = color
+    return tuple(foo)
 
 def get_rand_color():
     return (
@@ -18,13 +25,16 @@ def get_rand_color():
         )
         
 
-while color <= 255:
-    pos = 0
-    for row in range(0, ROWS):
-        for col in range(0, COLS):
-            np[pos] = (0, color, 0)
-            np.show()
-            pos += 1
-            
-        sleep(50)
-    color += 50
+while color_index < 3:
+    while color <= 255:
+        pos = 0
+        for row in range(0, ROWS):
+            for col in range(0, COLS):
+                np[pos] = get_next_color()
+                np.show()
+                pos += 1
+                
+            sleep(GAP)
+        color += 85
+    color_index += 1
+    color = 85
